d7d7335b187562def1163f72841a2f35ca21628c
kgpksh <tkdgns543@gmail.com>
2024-01-17 03:37:48 +0900

    첫 화면에서 로그인 여부 확인 구현

--- mynotes/lib/main.dart
+++ mynotes/lib/main.dart
@@ -1,5 +1,9 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:mynotes/views/login_view.dart';
+
+import 'firebase_options.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -9,7 +13,39 @@ void main() {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const LoginView(),
+      home: const HomePage(),
     ),
   );
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Home'),),
+      body: FutureBuilder(
+        future: Firebase.initializeApp(
+          options: DefaultFirebaseOptions.currentPlatform,
+        ),
+        builder: (context, snapshot) {
+          switch (snapshot.connectionState) {
+
+          // TODO: Handle this case.
+            case ConnectionState.done:
+              final user = FirebaseAuth.instance.currentUser;
+              if(user?.emailVerified ?? false) {
+                print('You are a varified user');
+              } else {
+                print('You need to verify your email first');
+              }
+              return const Text('Done');
+            default :
+              return const Text('Loading...');
+          }
+        },
+      ),
+    );
+  }
 }
