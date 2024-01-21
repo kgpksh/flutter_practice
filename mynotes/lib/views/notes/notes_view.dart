d3cf65e7405ca7271bfa3dd5ded47f59f5dd45f8
kgpksh <tkdgns543@gmail.com>
2024-01-22 00:22:37 +0900

    노트 내용 작성 기능 추가

--- mynotes/lib/views/notes/notes_view.dart
+++ mynotes/lib/views/notes/notes_view.dart
@@ -76,6 +76,8 @@ class _NoteViewState extends State<NoteView> {
                   builder: (context, snapshot) {
                     switch(snapshot.connectionState) {
                       case ConnectionState.waiting:
+
+                      case ConnectionState.active :
                         return const Text('Waiting for all notes...');
                       default :
                         return const CircularProgressIndicator();
