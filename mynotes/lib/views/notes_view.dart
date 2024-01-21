f3000a3c75575e52a0fd64bb863b59910a2d7b26
kgpksh <tkdgns543@gmail.com>
2024-01-21 17:43:34 +0900

    노트 뷰 타이틀 변경

--- mynotes/lib/views/notes_view.dart
+++ mynotes/lib/views/notes_view.dart
@@ -33,7 +33,7 @@ class _NoteViewState extends State<NoteView> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Main UI'),
+        title: const Text('Your Notes'),
         actions: [
           PopupMenuButton<MenuActions>(
             onSelected: (value) async {
