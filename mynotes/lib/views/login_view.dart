ff728497f834bd04b4e5db2c709b2825c4b268ea
kgpksh <tkdgns543@gmail.com>
2024-02-13 18:03:22 +0900

    로그인 및 인증에 bloc 적용 작업 마무리. Route 방식 bloc으로 대부분 정리

--- mynotes/lib/views/login_view.dart
+++ mynotes/lib/views/login_view.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:mynotes/constants/routes.dart';
 import 'package:mynotes/services/auth/auth_expectation.dart';
 import 'package:mynotes/services/auth/bloc/auth_bloc.dart';
 import 'package:mynotes/services/auth/bloc/auth_event.dart';
 import 'package:mynotes/services/auth/bloc/auth_state.dart';
+import 'package:mynotes/utilities/dialog/loading_dialog.dart';
 
 import '../utilities/dialog/error_dialog.dart';
 
@@ -18,6 +18,8 @@ class LoginView extends StatefulWidget {
 class _LoginViewState extends State<LoginView> {
   late final TextEditingController _email;
   late final TextEditingController _password;
+
+  CloseDialog? _closeDialogHandle;
 
   @override
   void initState() {
@@ -35,38 +37,48 @@ class _LoginViewState extends State<LoginView> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Login')),
-      body: Column(
-        children: [
-          TextField(
-            controller: _email,
-            enableSuggestions: false,
-            autocorrect: false,
-            keyboardType: TextInputType.emailAddress,
-            decoration:
-                const InputDecoration(hintText: 'Enter your email here'),
-          ),
-          TextField(
-            controller: _password,
-            obscureText: true,
-            enableSuggestions: false,
-            autocorrect: false,
-            decoration:
-                const InputDecoration(hintText: 'Enter your password here'),
-          ),
-          BlocListener<AuthBloc, AuthState>(
-            listener: (context, state) async {
-              if (state is AuthStateLoggedOut) {
-                if (state.exception
-                    is UserNotFoundOrWrongPasswordAuthException) {
-                  await showErrorDialog(context, 'User not found');
-                } else if (state.exception is GenericAuthException) {
-                  await showErrorDialog(context, 'Authentication Error');
-                }
-              }
-            },
-            child: TextButton(
+    return BlocListener<AuthBloc, AuthState>(
+      listener: (context, state) async {
+        if (state is AuthStateLoggedOut) {
+          final closeDialog = _closeDialogHandle;
+          if (!state.isLoading && closeDialog != null) {
+            closeDialog();
+            _closeDialogHandle = null;
+          } else if (state.isLoading && closeDialog == null) {
+            _closeDialogHandle = showLoadingDialog(
+              context: context,
+              text: 'Loading...',
+            );
+          }
+
+          if (state.exception is UserNotFoundOrWrongPasswordAuthException) {
+            await showErrorDialog(context, 'User not found');
+          } else if (state.exception is GenericAuthException) {
+            await showErrorDialog(context, 'Authentication Error');
+          }
+        }
+      },
+      child: Scaffold(
+        appBar: AppBar(title: const Text('Login')),
+        body: Column(
+          children: [
+            TextField(
+              controller: _email,
+              enableSuggestions: false,
+              autocorrect: false,
+              keyboardType: TextInputType.emailAddress,
+              decoration:
+                  const InputDecoration(hintText: 'Enter your email here'),
+            ),
+            TextField(
+              controller: _password,
+              obscureText: true,
+              enableSuggestions: false,
+              autocorrect: false,
+              decoration:
+                  const InputDecoration(hintText: 'Enter your password here'),
+            ),
+            TextButton(
               onPressed: () async {
                 final email = _email.text;
                 final password = _password.text;
@@ -79,16 +91,15 @@ class _LoginViewState extends State<LoginView> {
               },
               child: const Text('Login'),
             ),
-          ),
-          TextButton(
-              onPressed: () {
-                Navigator.of(context).pushNamedAndRemoveUntil(
-                  registerRoute,
-                  (route) => false,
-                );
-              },
-              child: const Text('Not registered yet? Register here!'))
-        ],
+            TextButton(
+                onPressed: () {
+                  context.read<AuthBloc>().add(
+                        const AuthEventShouldRegister(),
+                      );
+                },
+                child: const Text('Not registered yet? Register here!'))
+          ],
+        ),
       ),
     );
   }
--- mynotes/lib/views/verify_email_view.dart
+++ mynotes/lib/views/verify_email_view.dart
@@ -1,9 +1,7 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'dart:developer' as devtools show log;
-
-import 'package:mynotes/constants/routes.dart';
-import 'package:mynotes/services/auth/auth_service.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:mynotes/services/auth/bloc/auth_bloc.dart';
+import 'package:mynotes/services/auth/bloc/auth_event.dart';
 
 class VerifyEmailView extends StatefulWidget {
   const VerifyEmailView({super.key});
@@ -16,27 +14,32 @@ class _VerifyEmailViewState extends State<VerifyEmailView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Verify Email'),),
+      appBar: AppBar(
+        title: const Text('Verify Email'),
+      ),
       body: Column(
         children: [
-          const Text("We've sent an email verifiction. Please open it to verify your account."),
-          const Text("If you haven't receive a verification email yet, press the button below."),
+          const Text(
+              "We've sent an email verifiction. Please open it to verify your account."),
+          const Text(
+              "If you haven't receive a verification email yet, press the button below."),
           TextButton(
-              onPressed: () async {
-                await AuthService.firebase().sendEmailVerification();
+              onPressed: () {
+                context.read<AuthBloc>().add(
+                      const AuthEventSendEmailVerification(),
+                    );
               },
               child: const Text('Send an email address')),
           TextButton(
-            onPressed: () async {
-              AuthService.firebase().logOut();
-              if(!context.mounted) return;
-              Navigator.of(context).pushNamedAndRemoveUntil(
-                  registerRoute,
-                  (route) => false
-              );
-          },
-            child: const Text('Restart'),)
-        ],),
+            onPressed: () {
+              context.read<AuthBloc>().add(
+                    const AuthEventLogout(),
+                  );
+            },
+            child: const Text('Restart'),
+          )
+        ],
+      ),
     );
   }
 }
