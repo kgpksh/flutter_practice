986c88bcc97760aa73e33c4d43e3e982f8dd6810
kgpksh <tkdgns543@gmail.com>
2024-02-19 01:06:58 +0900

    파이어베이스 노트 선택 코드 작성자 것만 가져오게 필터링 구현

--- mynotes/lib/services/cloud/firebase_cloud_storage.dart
+++ mynotes/lib/services/cloud/firebase_cloud_storage.dart
@@ -9,7 +9,7 @@ class FirebaseCloudStorage {
   Future<void> deleteNote({required String documentID}) async {
     try {
       notes.doc(documentID).delete();
-    } catch(e) {
+    } catch (e) {
       throw CouldNotDeleteNoteException();
     }
   }
@@ -19,29 +19,18 @@ class FirebaseCloudStorage {
     required String text,
   }) async {
     try {
-      notes.doc(documentId).update({textFieldName : text});
-    }catch(e) {
+      notes.doc(documentId).update({textFieldName: text});
+    } catch (e) {
       throw CouldNotUpdateNoteException();
     }
   }
 
-  Stream<Iterable<CloudNote>> allNotes({required String ownerUserId}) =>
-    notes.snapshots().map((event) => event.docs
-        .map((doc) => CloudNote.fromSnapshot(doc))
-        .where((note) => note.ownerUserId == ownerUserId));
-
-  Future<Iterable<CloudNote>> getNotes({required String ownerUserId}) async {
-    try {
-      return await notes.where(
-        ownerUserIdFieldName,
-        isEqualTo: ownerUserId,
-      )
-      .get()
-      .then((value) => value.docs.map((doc) => CloudNote.fromSnapshot(doc),),
-      );
-    } catch(e) {
-      throw CouldNotGetAllNotesException();
-    }
+  Stream<Iterable<CloudNote>> allNotes({required String ownerUserId}) {
+    final allNotes = notes
+        .where(ownerUserIdFieldName, isEqualTo: ownerUserId)
+        .snapshots()
+        .map((event) => event.docs.map((doc) => CloudNote.fromSnapshot(doc)));
+    return allNotes;
   }
 
   Future<CloudNote> createNewNote({required String ownerUserId}) async {
